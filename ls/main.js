66fcf90bfe82bdc86a51cffbc8085756e5e149f4
Arnar Yngvason <arnar@hvitahusid.is>
2013-06-19 10:35:24 +0000

    init

---
+++ ls/main.js
@@ -0,0 +1,49 @@
+$(function(){
+  var instafeed;
+  instafeed = new Instafeed({
+    clientId: '3d26c3c843b34294954a8a0abbf9a1d0',
+    get: 'tagged',
+    tagName: 'skyr',
+    mock: true,
+    limit: 60,
+    success: function(data){
+      var feed, i$, ref$, len$, item, caption, e, _from, this$ = this;
+      console.log(data);
+      feed = [];
+      for (i$ = 0, len$ = (ref$ = data.data).length; i$ < len$; ++i$) {
+        item = ref$[i$];
+        try {
+          caption = item.caption.text.split(/\ #|#/).join(' #').replace(/ +/g, " ");
+        } catch (e$) {
+          e = e$;
+          caption = '';
+        }
+        try {
+          _from = item.caption.from.full_name;
+        } catch (e$) {
+          e = e$;
+          _from = '';
+        }
+        feed.push({
+          link: item.link,
+          from: _from,
+          thumb: item.images.low_resolution.url,
+          caption: caption
+        });
+      }
+      return dust.render("feed", {
+        feed: feed
+      }, function(err, out){
+        return $('#feed').html(out);
+      });
+    }
+  });
+  instafeed.error = function(){
+    try {
+      return instafeed.run();
+    } catch (e$) {}
+  };
+  try {
+    return instafeed.run();
+  } catch (e$) {}
+});
---
+++ ls/main.ls
@@ -0,0 +1,39 @@
+$ ->
+    instafeed = new Instafeed do
+        clientId: '3d26c3c843b34294954a8a0abbf9a1d0',
+        get: 'tagged',
+        tagName: 'skyr',
+        mock: true,
+        limit: 60,
+        success: (data) ->
+            console.log(data)
+
+            feed = []
+            for item in data.data
+                # Make sure there are spaces between tags and remove multiple spaces:
+                try
+                    caption = item.caption.text.split(/\ #|#/).join(' #').replace(/ +/g, " ")
+                catch
+                    caption = ''
+
+                try
+                    _from = item.caption.from.full_name
+                catch
+                    _from = ''
+
+                # Append relative data to feed for template:
+                feed.push do
+                    link: item.link
+                    from: _from
+                    thumb: item.images.low_resolution.url
+                    caption: caption
+
+            dust.render "feed", {feed: feed}, (err, out) ~>
+                $('#feed').html(out)
+
+    instafeed.error = ->
+        try
+            instafeed.run()
+
+    try
+        instafeed.run()
